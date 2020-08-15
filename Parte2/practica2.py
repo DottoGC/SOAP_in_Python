301bad72a014bf8629d808ea2445ad0e8f4f5e40
DottoGC <otttoguitar@gmail.com>
2020-08-15 00:19:59 -0600

    actualizacion README.md

--- Parte2/practica2.py
+++ Parte2/practica2.py
@@ -1,25 +1,31 @@
-#IMPORTACION DE LIBRERIA USADA LLAMADA ZEEP
-from zeep  import Client 
-
-#CREACION VARIABLE QUE REFERENCIA AL WEB SERVICES
-client = Client(wsdl="https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl") 
-
-				
-#CICLO QUE UTILIZA FUNCION PARA OBTENER CLIENTES
-for c in client.service.readList():    
-        print(c.name) 
+# IMPORTACION DE LIBRERIAS
+from requests.auth import HTTPBasicAuth
+from requests import Session
+from zeep import Client
+from zeep.transports import Transport
 
 
+# DECLARACION DE VARIABLES NECESARIAS PARA LA SOLUCION
+session = Session()
+user = 'sa'
+password = 'usac'
 
-#CICLO QUE UTILIZA FUNCION PARA INGRESAR 10 CLIENTES SOLICITADOS
-#c = 0 
-#for i in range(10): 
-#    if c < 10: 
-#        print("ingreasndo... ",client.service.create("201020975_"+str(c))) 
-#    c +=1 
+# UTILIZACION DE METODO AUTH DE LA SESION CREADA COMO VARIABLE ANTERIORMENTE
+session.auth = HTTPBasicAuth(user, password)
+
+# CODIGO PARA REALIZAR REQUEST CON TODOS LAS VARIABLES NECESARIAS DE AUTENTICACION
+client = Client(wsdl='https://api.softwareavanzado.world/index.php?webserviceClient=administrator&webserviceVersion=1.0.0&option=contact&api=soap&wsdl',
+            transport=Transport(session=session))
 
 
+# BUCLE FOR PARA CREACION DE OBJETOS
+c = 0
+for i in range(10):
+    if c<10:
+        print("ingresando...",client.service.create("201020975-basicA-"+str(c))) 
+    c+=1
 
-#CICLO QUE UTILIZA FUNCION PARA OBTENER CLIENTES ESPECIFICOS ENVIANDO PARAMETROS
-#for c in client.service.readList(0,10,201020975):    
-#        print(c.name) 
+        
+#CICLO QUE UTILIZA FUNCION PARA OBTENER CLIENTES ESPECIFICOS ENVIANDO PARAMETROS PARA UN FILTRADO
+for c in client.service.readList(0,10,"201020975-basicA"):    
+    print(c.name)
